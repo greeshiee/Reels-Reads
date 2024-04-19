e6b3de98b0de03c395d72299ab542108e2d655a9
greeshie <grepra17@gmail.com>
2024-04-19 14:57:53 -0500

    log in with php

---
+++ src/login.php
@@ -0,0 +1,22 @@
+<?php 
+
+   include 'connect.php';
+
+    if(isset($_POST['logIn'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+        $sql = "SELECT * FROM USER_INFO WHERE email='$email' AND pass='$password'";
+        $result = $conn->query($sql);
+        if($result->num_rows>0){
+            session_start();
+            $row=$result->fetch_assoc();
+            $_SESSION['email']=$row['email'];
+            header("Location: home.html");
+            exit();
+        }
+        else{
+            echo "Account not found!";
+        }
+    }
+?>
